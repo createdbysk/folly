4a272829c1b80616bf117e91ca32ee6863a01127
Satish Kumar <kumarnotifications@gmail.com>
2025-12-17 21:46:51 -0800

    Add --api-only support to setup.py (mirrors thrift pattern)
    - setup.py --api-only: generates API headers only, no linking
    - CMakeLists.txt uses --api-only for header generation phase
    - Build order: headers -> folly_python_cpp -> python extensions

--- folly/python/setup.py
+++ folly/python/setup.py
@@ -18,6 +18,9 @@
 # Cython requires source files in a specific structure, the structure is
 # created as tree of links to the real source files.
 
+import sys
+
+import Cython
 from Cython.Build import cythonize
 from Cython.Compiler import Options
 from setuptools import Extension, setup
@@ -25,25 +28,41 @@ from setuptools import Extension, setup
 
 Options.fast_fail = True
 
-exts = [
-    Extension(
-        "folly.executor",
-        sources=["folly/executor.pyx", "folly/ProactorExecutor.cpp"],
-        libraries=["folly_python_cpp", "folly", "glog"],
-    ),
-    Extension(
-        "folly.iobuf",
-        sources=["folly/iobuf.pyx", "folly/iobuf_ext.cpp"],
-        libraries=["folly_python_cpp", "folly", "glog"],
-    ),
-]
+if "--api-only" in sys.argv:
+    # Generate API headers only (no C++ compilation or linking)
+    # Used by CMake to generate iobuf_api.h before building folly_python_cpp
+    Cython.Compiler.Main.compile(
+        "folly/iobuf.pyx",
+        full_module_name="folly.iobuf",
+        cplus=True,
+        language_level=3,
+    )
+    Cython.Compiler.Main.compile(
+        "folly/executor.pyx",
+        full_module_name="folly.executor",
+        cplus=True,
+        language_level=3,
+    )
+else:
+    exts = [
+        Extension(
+            "folly.executor",
+            sources=["folly/executor.pyx", "folly/ProactorExecutor.cpp"],
+            libraries=["folly_python_cpp", "folly", "glog"],
+        ),
+        Extension(
+            "folly.iobuf",
+            sources=["folly/iobuf.pyx", "folly/iobuf_ext.cpp"],
+            libraries=["folly_python_cpp", "folly", "glog"],
+        ),
+    ]
 
-setup(
-    name="folly",
-    version="0.0.1",
-    packages=["folly"],
-    package_data={"": ["*.pxd", "*.h"]},
-    setup_requires=["cython"],
-    zip_safe=False,
-    ext_modules=cythonize(exts, compiler_directives={"language_level": 3}),
-)
+    setup(
+        name="folly",
+        version="0.0.1",
+        packages=["folly"],
+        package_data={"": ["*.pxd", "*.h"]},
+        setup_requires=["cython"],
+        zip_safe=False,
+        ext_modules=cythonize(exts, compiler_directives={"language_level": 3}),
+    )
